ae2d15fadd82c0c97bed6f0310c2a5f1a8a48f83
Ana Carolina Ataliba <carulinaataliba@gmail.com>
2021-08-02 22:24:31 -0300

    página home e rotas

---
+++ mobile/src/Screens/Favoritos/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Favoritos() {
+    return (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>Favoritos Screen</Text>
+        </View>
+    )
+}
---
+++ mobile/src/Screens/Presente/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Presentes() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Dicas de presentes Screen</Text>
+    </View>
+  );
+}
